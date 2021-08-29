f286c5a09fa6ab17d87650a624ff63abf5019b00
Ahmad Kholid <kholid060@gmail.com>
2021-08-29 20:48:23 +0800

    feat: allow drop images in editor

---
+++ packages/renderer/src/utils/copy-image.js
@@ -0,0 +1,23 @@
+import sha256 from 'crypto-js/sha256';
+import { useStorage } from '@/composable/storage';
+
+const storage = useStorage('settings');
+const { path, ipcRenderer } = window.electron;
+
+export default async function (filePath, id) {
+  try {
+    const dataDir = await storage.get('dataDir');
+    const { ext, name } = path.parse(filePath);
+    const fileName = sha256(name).toString() + ext;
+    const destPath = path.join(dataDir, 'notes-assets', id, fileName);
+
+    await ipcRenderer.callMain('fs:copy', {
+      path: filePath,
+      dest: destPath,
+    });
+
+    return { destPath, fileName };
+  } catch (error) {
+    console.error(error);
+  }
+}
