a8e5ccd1ff669cadce379fa411525709c8188fe0
Ahmad Kholid <kholid060@gmail.com>
2021-09-03 17:49:08 +0800

    feat: add task list

--- packages/renderer/src/lib/tiptap/index.js
+++ packages/renderer/src/lib/tiptap/index.js
@@ -6,6 +6,8 @@ import Underline from '@tiptap/extension-underline';
 import Link from '@tiptap/extension-link';
 import Code from '@tiptap/extension-code';
 import Placeholder from '@tiptap/extension-placeholder';
+import TaskList from '@tiptap/extension-task-list';
+import TaskItem from '@tiptap/extension-task-item';
 import LabelSuggestion from './exts/label-suggestion';
 import MathInline from './exts/math-inline';
 import MathBlock from './exts/math-block';
@@ -21,6 +23,8 @@ export const extensions = [
   Underline,
   LinkNote,
   LabelSuggestion,
+  TaskList,
+  TaskItem,
   CodeBlock,
   MathInline,
   MathBlock,
